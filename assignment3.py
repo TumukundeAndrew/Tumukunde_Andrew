4d1ec56874c4d3d1012cfec66858b128550e29f6
TumukundeAndrew <tumukundeandrew07@gmail.com>
2025-05-23 22:04:06 +0300

    changes to assignment3

--- assignment3.py
+++ assignment3.py
@@ -1,15 +1,17 @@
+def divide_numbers(n, m):
+    return n / m  
+
 while True:
-  try:
-    number1 = int(input("Enter number1: "))
-    number2 = int(input("Enter number2: "))
-    
-    result = number1/number2
-    
-  except ValueError:
-    print("invalid number! enter a valid interger.")
-  except ZeroDivisionError:
-    print("division by zero is not allowed. enetr a non zero number")
-  else:
-    print(f"{number1}/{number2} = {result}")
-    break                  
+    try:
+        number1 = int(input("Enter number1: "))
+        number2 = int(input("Enter number2: "))
         
+        result = divide_numbers(number1, number2)
+        
+    except ValueError:
+        print("Invalid number! Enter a valid integer.")
+    except ZeroDivisionError:
+        print("Division by zero is not allowed. Enter a non-zero number.")
+    else:
+        print(f"The result is: {result}")
+        break  
